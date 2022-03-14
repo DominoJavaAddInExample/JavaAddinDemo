35d1c75247b360fe8308f8b756641ef1f62dc2cf
Dmytro <dpastov@gmail.com>
2022-03-14 14:24:33 +0100

    up version

--- src/main/java/JavaAddinDemo.java
+++ src/main/java/JavaAddinDemo.java
@@ -1,23 +1,4 @@
-import java.util.Calendar;
-
 public class JavaAddinDemo extends JavaServerAddinGenesis {
-	
-	protected void showHelp() {
-		int year = Calendar.getInstance().get(Calendar.YEAR);
-		logMessage("*** Usage ***");
-		AddInLogMessageText("load runjava JavaAddinDemo");
-		AddInLogMessageText("tell JavaAddinDemo <command>");
-		AddInLogMessageText("   quit       Unload JavaAddinDemo");
-		AddInLogMessageText("   help       Show help information (or -h)");
-		AddInLogMessageText("   info       Show version and more of JavaAddinDemo (or -i)");
-		AddInLogMessageText("Copyright (C) Prominic.NET, Inc. 2021" + (year > 2021 ? " - " + Integer.toString(year) : ""));
-		AddInLogMessageText("See https://prominic.net for more details.");
-	}
-
-	@Override
-	protected String getJavaAddinName() {
-		return "JavaAddinDemo";
-	}
 
 	@Override
 	protected String getJavaAddinVersion() {
@@ -26,7 +7,7 @@ public class JavaAddinDemo extends JavaServerAddinGenesis {
 
 	@Override
 	protected String getJavaAddinDate() {
-		return "2022-03-09 16:05";
+		return "2022-03-14 16:05";
 	}
 
 	@Override
