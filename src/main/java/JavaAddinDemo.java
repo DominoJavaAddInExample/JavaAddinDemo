cec0381a13eaf9255ec284fbf041e6851e43e6a6
Dmytro <dpastov@gmail.com>
2022-04-05 11:28:18 +0200

    up version

--- src/main/java/JavaAddinDemo.java
+++ src/main/java/JavaAddinDemo.java
@@ -1,15 +1,15 @@
-import net.prominic.gja_v20220330.JavaServerAddinGenesis;
+import net.prominic.gja_v20220405.JavaServerAddinGenesis;
 
 public class JavaAddinDemo extends JavaServerAddinGenesis {
 
 	@Override
 	protected String getJavaAddinVersion() {
-		return "1.2.5";
+		return "1.2.6";
 	}
 
 	@Override
 	protected String getJavaAddinDate() {
-		return "2022-03-30 16:05";
+		return "2022-04-05 16:05";
 	}
 
 	@Override
